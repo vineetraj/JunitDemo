f7c2edb8480f1cbaa77156369c817ae08b95d747
Vineet Raj <vineetraj14@gmail.com>
2023-01-18 01:22:18 +0530

    Moved code inside nested classes

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -16,13 +16,14 @@ import java.util.List;
 class ContactManagerTest {
 
     ContactManager contactManager;
+
     @BeforeAll
-    public void setupAll(){ //it must be a static method. It's executed for whole test class once
+    public void setupAll() { //it must be a static method. It's executed for whole test class once
         System.out.println("Should print before all test cases");
     }
 
     @BeforeEach
-    public void Setup(){ //each test method will have its own copy of contactManager
+    public void Setup() { //each test method will have its own copy of contactManager
         contactManager = new ContactManager();
     }
 
@@ -70,7 +71,7 @@ class ContactManagerTest {
     /*Conditional Executions*/
     @Test
     @DisplayName("should not run on windows")
-    @DisabledOnOs(value=OS.WINDOWS,disabledReason = "no need to run on windows")
+    @DisabledOnOs(value = OS.WINDOWS, disabledReason = "no need to run on windows")
     public void shouldNotCreateContactOnWindows() {
         contactManager.addContact("Vikash", "Raj", "0955621478");
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty()); //we expect contact list shouldn't be empty
@@ -79,30 +80,65 @@ class ContactManagerTest {
 
     @Test
     @DisplayName("should not run on Mac os")
-    @DisabledOnOs(value=OS.MAC,disabledReason = "no need to run on Mac")
+    @DisabledOnOs(value = OS.MAC, disabledReason = "no need to run on Mac")
     public void shouldNotCreateContactsOnMac() {
         contactManager.addContact("Rajoo", "Jaiswal", "0944621478");
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty()); //we expect contact list shouldn't be empty
         Assertions.assertEquals(1, contactManager.getAllContacts().size());
     }
 
-    /*Repeated Tests*/
-    @DisplayName("repeat contact creation 5 times")
-    @RepeatedTest(value = 5, name = "Current repetition is {currentRepetition} of total {totalRepetitions}")
-    public void shouldCreateContactRepeatedly() {
-        contactManager.addContact("Vineet", "Raj", "0956051722");
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty()); //we expect contact list shouldn't be empty
-        Assertions.assertEquals(1, contactManager.getAllContacts().size());
+    @AfterEach
+    public void tearDown() {
+        System.out.println("should be executed after each test");
     }
 
-    /*Using Value Sources*/
-    @DisplayName("Value Source case")
-    @ParameterizedTest
-    @ValueSource(strings={"0123456789","0321654987","9874564321"})
-    public void shouldCreateContactRepeatedlyUsingValueSource(String phnNums) {
-        contactManager.addContact("Vineet", "Raj", phnNums);
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
-        Assertions.assertEquals(1, contactManager.getAllContacts().size());
+    @AfterAll
+    public void tearDownAll() { //must be static
+        System.out.println("should be executed at the end");
+    }
+
+    @Nested
+    class RepeatedNestedTests {
+        /*Repeated Tests*/
+        @DisplayName("repeat contact creation 5 times")
+        @RepeatedTest(value = 5, name = "Current repetition is {currentRepetition} of total {totalRepetitions}")
+        public void shouldCreateContactRepeatedly() {
+            contactManager.addContact("Vineet", "Raj", "0956051722");
+            Assertions.assertFalse(contactManager.getAllContacts().isEmpty()); //we expect contact list shouldn't be empty
+            Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        }
+    }
+
+    @Nested
+    class ParametrisedNestedTests {
+        /*Using Value Sources*/
+        @DisplayName("Value Source case")
+        @ParameterizedTest
+        @ValueSource(strings = {"0123456789", "0321654987", "9874564321"})
+        public void shouldCreateContactRepeatedlyUsingValueSource(String phnNums) {
+            contactManager.addContact("Vineet", "Raj", phnNums);
+            Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+            Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        /*using CSV Sources*/
+        @DisplayName("Csv Source case")
+        @ParameterizedTest
+        @CsvSource({"0123456789", "0321654987", "9874564321"})
+        public void shouldCreateContactRepeatedlyUsingCSVSource(String phnNums) {
+            contactManager.addContact("Vineet", "Raj", phnNums);
+            Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+            Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        }
+
+        @DisplayName("CSV File Source case")
+        @ParameterizedTest
+        @CsvFileSource(resources = "/data.csv")
+        public void shouldCreateContactRepeatedlyUsingCSVFileSource(String phnNums) {
+            contactManager.addContact("Vineet", "Raj", phnNums);
+            Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+            Assertions.assertEquals(1, contactManager.getAllContacts().size());
+        }
     }
 
     /*Using Method Sources*/
@@ -115,37 +151,7 @@ class ContactManagerTest {
         Assertions.assertEquals(1, contactManager.getAllContacts().size());
     }
 
-    /*using CSV Sources*/
-    @DisplayName("Csv Source case")
-    @ParameterizedTest
-    @CsvSource({"0123456789","0321654987","9874564321"})
-    public void shouldCreateContactRepeatedlyUsingCSVSource(String phnNums) {
-        contactManager.addContact("Vineet", "Raj", phnNums);
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
-        Assertions.assertEquals(1, contactManager.getAllContacts().size());
+    private static List<String> source() {
+        return Arrays.asList("0123456789", "0321654987", "9874564321");
     }
-
-    private static List<String > source(){
-        return Arrays.asList("0123456789","0321654987","9874564321");
-    }
-
-    @DisplayName("CSV File Source case")
-    @ParameterizedTest
-    @CsvFileSource(resources = "/data.csv")
-    public void shouldCreateContactRepeatedlyUsingCSVFileSource(String phnNums) {
-        contactManager.addContact("Vineet", "Raj", phnNums);
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
-        Assertions.assertEquals(1, contactManager.getAllContacts().size());
-    }
-
-    @AfterEach
-    public void tearDown(){
-        System.out.println("should be executed after each test");
-    }
-    @AfterAll
-    public void tearDownAll() { //must be static
-        System.out.println("should be executed at the end");
-    }
-
-
 }
